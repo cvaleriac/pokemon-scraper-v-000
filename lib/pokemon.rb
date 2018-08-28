adbebf2ade9299337517ff9ab19735214e7a6fa9
valeria campilongo <cvaleriac@hotmail.it>
2018-08-28 19:46:32 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,6 +16,12 @@ class Pokemon
   def self.find (id, db)
     statement = db.prepare("SELECT * FROM pokemon WHERE id = ?")
     result_set = statement.execute(id)
+    results = result_set.collect do |row|
+      pokemon = Pokemon.new(name: row[1], type: row[2], db: db, id: id)
+      pokemon.hp = row[3]
+      pokemon
+    end
+    results[0]
   end
 
 end
