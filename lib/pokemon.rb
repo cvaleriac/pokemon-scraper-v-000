dd3d85a80bf57c1848501c3011520ef39244025a
valeria campilongo <cvaleriac@hotmail.it>
2018-08-28 19:41:30 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,11 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize (name, type, db)
+  def initialize(name:, type:, db:, id:)
     @name = name
     @type = type
     @db = db
+    @id = id
   end
 
 end
